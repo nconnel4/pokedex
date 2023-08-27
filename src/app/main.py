568c18f15a5a3432eeec7781876ba6aa0ae2133f
nconnel4 <nconnel4@hotmail.com>
2023-08-27 19:55:45 -0400

    Chore: remove unneeded settings

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
 
-from app.core.config import Settings
-
 app = FastAPI()
-
-settings = Settings()
 
 
 @app.get("/")
 async def ping() -> dict[str, str]:
-    print(settings.database_url)
     return {"message": "pong!"}
